9a940a05c3e96b4a008b57afcdf71d3f9c02eb14
Navo Bogoda <navobogoda0717@gmail.com>
2026-05-11 11:32:21 +0530

    Update event controller

--- controller/eventController.js
+++ controller/eventController.js
@@ -4,3 +4,20 @@ exports.createEvent = async (req, res) => {
   const event = await Event.create(req.body);
   res.json(event);
 };
+
+exports.getEvents = async (req, res) => {
+  const events = await Event.find();
+  res.json(events);
+};
+
+
+exports.updateEvent = async (req, res) => {
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(event);
+};
+
+
+exports.deleteEvent = async (req, res) => {
+  await Event.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+};
