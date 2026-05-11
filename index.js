9555db9167bfb26953afd01a495055e19e89f35a
Navo Bogoda <navobogoda0717@gmail.com>
2026-05-11 12:07:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import eventRoutes from "./routes/eventRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
+mongoose.connect("mongodb://127.0.0.1:27017/eventapi")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
-app.listen(5000, () => console.log("Server running"));
+app.get("/", (req, res) => {
+  res.send("Student Event API Running...");
+});
+
+app.use("/api/events", eventRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
