2ed902c85f47cbd8d1503fc873446dbdb00c5273
Navo Bogoda <navobogoda0717@gmail.com>
2026-05-11 13:42:04 +0530

    Create Get method
    Create get method in registrationController and registrationRoutes

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -4,3 +4,8 @@ exports.registerEvent = async (req, res) => {
   const reg = await Registration.create(req.body);
   res.json(reg);
 };
+
+exports.getRegistrations = async (req, res) => {
+  const regs = await Registration.find();
+  res.json(regs);
+};
--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const ctrl = require("../controllers/registrationController");
 
 router.post("/", ctrl.registerEvent);
+router.get("/", ctrl.getRegistrations);
+
 
 module.exports = router;
