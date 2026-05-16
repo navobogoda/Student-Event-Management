ed210909d086e30b2a210ea9b6ed08f04d6a80cd
Navo Bogoda <navobogoda0717@gmail.com>
2026-05-16 19:42:50 +0530

    create .env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 import eventRoutes from "./routes/eventRoutes.js";
@@ -11,7 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/eventapi")
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
